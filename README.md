1a38cb9e65df36497edfcece73a290a7492cc638
FrankJamison <frank@frankjamison.com>
2025-12-22 01:16:15 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Jamison Stamps & Books
+
+This is the business website that I use to sell stamps on the internet.
+
+This site features a JavaScript product loader that uses PayPal as the shopping service.
---
+++ js/footer.js
@@ -0,0 +1,16 @@
+// FOOTER
+
+document.write('<div id="footerdiv">');
+
+document.write('&copy; Copyright 2025 <a href="index.html" class="footerlink">Jamison Enterprises Stamps &amp; Books</a> ');
+
+// document.write('// Site Design &copy; <a href="https://allwebcodesign.com" rel="nofollow" class="footerlink">Allwebco Design</a><br> ');
+
+document.write('</div>');
+
+
+
+
+// NEXT LINE IS SCROLL TO TOP ARROW
+
+document.write('<a href="#" class="scrollToTop"></a>');
